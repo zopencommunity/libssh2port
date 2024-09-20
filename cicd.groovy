cdd39a39ea60920b1b8ffa766e104e75e959ff21
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:43 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/libssh2port.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/libssh2port.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libssh2port.git'), string(name: 'PORT_DESCRIPTION', value: 'libssh2 is a client-side C library implementing the SSH2 protocol' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libssh2port.git'), string(name: 'PORT_DESCRIPTION', value: 'libssh2 is a client-side C library implementing the SSH2 protocol' )]
   }
 }
